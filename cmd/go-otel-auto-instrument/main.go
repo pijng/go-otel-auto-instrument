e7e201ff628f92c91ae144ca219d6ae4c74e9cf8
Mark <shalpack@gmail.com>
2024-11-29 10:09:02 +0300

    Add line directives to adjust source code ref

--- cmd/go-otel-auto-instrument/main.go
+++ cmd/go-otel-auto-instrument/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"go/token"
 	"strconv"
+	"strings"
 
 	"github.com/dave/dst"
 	"github.com/dave/dst/decorator"
 	"github.com/pijng/goinject"
 )
+
+// lnOffset is used to calculate the line number of the function where instrumentation occurs.
+// The current offset value MUST equal the number of lines added by the instrumentation + 1.
+// The calculated value will then be used as a /*line */ directive.
+const lnOffset = 5 + 1
 
 type autoinstrumentModifier struct{}
 
@@ -31,22 +38,29 @@ func (mm autoinstrumentModifier) Modify(f *dst.File, dec *decorator.Decorator, r
 		// 	}
 		// }
 
-		// for _, param := range funcDecl.Type.Params.List {
-		// 	starExpr, isPointer := param.Type.(*dst.StarExpr)
-		// 	if !isPointer {
-		// 		continue
-		// 	}
-		// 	paramIdent, isIdent := starExpr.X.(*dst.Ident)
-		// 	if !isIdent {
-		// 		continue
-		// 	}
+		for _, param := range funcDecl.Type.Params.List {
+			starExpr, isPointer := param.Type.(*dst.StarExpr)
+			if !isPointer {
+				continue
+			}
+			paramIdent, isIdent := starExpr.X.(*dst.Ident)
+			if !isIdent {
+				continue
+			}
 
-		// 	if paramIdent.Path == "net/http" && paramIdent.Name == "Request" && param.Names[0].Name != "_" {
-		// 		contextArgName = param.Names[0].Name + ".Context()"
-		// 	}
-		// }
+			if paramIdent.Path == "net/http" && paramIdent.Name == "Request" && param.Names[0].Name != "_" {
+				contextArgName = param.Names[0].Name + ".Context()"
+			}
+		}
 
 		funcName := f.Name.Name + "." + funcDecl.Name.Name
+
+		line, err := getLine(dec, funcDecl.Type)
+		if err != nil {
+			panic(err)
+		}
+
+		addLineDirectiveToFuncDecl(funcDecl, line, lnOffset)
 
 		spanStmt := buildSpan(funcName, contextArgName)
 		funcDecl.Body.List = append(spanStmt.List, funcDecl.Body.List...)
@@ -74,7 +88,7 @@ func buildSpan(funcName string, contextArgName string) dst.BlockStmt {
 	lhs := make([]dst.Expr, 0)
 
 	newCtxName := contextArgName
-	if newCtxName == "" {
+	if newCtxName == "" || strings.Contains(newCtxName, ".") {
 		newCtxName = "_"
 	}
 	oldCtxName := "goai_oldCtx"
@@ -127,4 +141,18 @@ func buildSpan(funcName string, contextArgName string) dst.BlockStmt {
 			},
 		},
 	}
+}
+
+func addLineDirectiveToFuncDecl(funcDecl *dst.FuncDecl, funcLine int, offset int) {
+	funcDecl.Decs.NodeDecs.Start.Append(fmt.Sprintf("/*line :%d:1*/", funcLine-offset))
+}
+
+func getLine(dec *decorator.Decorator, funcType *dst.FuncType) (int, error) {
+	astNode := dec.Map.Ast.Nodes[funcType]
+	if astNode != nil {
+		pos := astNode.Pos()
+		return dec.Fset.Position(pos).Line, nil
+	}
+
+	return 0, fmt.Errorf("could not find a position for: %v", funcType)
 }
